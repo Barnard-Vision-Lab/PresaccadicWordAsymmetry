7242f3975902d4a56f67d683b3c70f839c7b864b
Mariam Latif <135742396+mariamlatif@users.noreply.github.com>
2025-06-17 20:24:17 -0400

    function looks for .edf, ignores practice folders

---
+++ analysis/edfFilesNoPractice.m
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function loops though all edf files and does not include practice
+% folders 
+%
+% Made for PWA analysis code June 2025
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function noPracticeEdf = edfFilesNoPractice(participantPath)
+
+%% NOTE: Does not use getFilesByType (but can - maybe implement later)
+    % Recursively finds all .edf files under participantPath excluding 'practice' folders
+    allDirs = dir(fullfile(participantPath, '**', '*.edf'));
+    noPracticeEdf = {};
+    
+    for i = 1:length(allDirs)
+        thisPath = fullfile(allDirs(i).folder, allDirs(i).name);
+        if ~contains(thisPath, [filesep 'practice' filesep]) && ...
+           ~contains(thisPath, [filesep 'practice'])
+            noPracticeEdf{end+1} = thisPath;
+        end
+    end
+end
